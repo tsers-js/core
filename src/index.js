3b7a684bea989a42b0062932da19d2412a7db413
Matti Lankinen <m.lankinen@iki.fi>
2016-03-08 12:01:48 +0200

    Fix some bugs and add tests

--- src/index.js
+++ src/index.js
@@ -3,50 +3,56 @@ import Rx, {Observable as O} from "rx"
 const keys = Object.keys
 
 const fromKey = signal$ => key =>
-  signal$.filter(s => s.key === key).map(s => s.value)
+  signal$.filter(s => s.key === key).map(s => s.val)
 
-const input = signal$ => Object.assign(fromKey(signal$), {
-  keys: (...keys) => keys.reduce((o, k) => ({...o, [k]: fromKey(signal$)(k)}), {})
-})
 
-const output = (output, local) =>
+const mergeObj = (output, local = false) =>
   O.merge(keys(output).map(key => output[key].map(val => ({val, key, local}))))
 
+export const signals = signal$ => ({
+  of: fromKey(signal$),
+  ofKeys: (...keys) => keys.reduce((o, k) => ({...o, [k]: fromKey(signal$)(k)}), {})
+})
 
-export default env => {
+export const drivers = spec => {
   const compact = (obj, fn) =>
     keys(obj).reduce((o, k) => { let a = fn(obj[k]); return a ? ({...o, [k]: a}) : o}, {})
 
-  const drivers = compact(env, f => f())
+  const drivers = compact(spec, f => f())
   return {
-    signals: output(compact(drivers, d => d.signals), false),
+    signals: mergeObj(compact(drivers, d => d.signals), false),
     transforms: compact(drivers, d => d.transforms),
-    interpreters: compact(drivers, d => d.interpreter)
+    executors: compact(drivers, d => d.executor)
   }
 }
 
 export const run = (signal$, main) => {
-  let sig = null
+  let loop = null
   const input$ = signal$
     .filter(s => !s.local)
-    .merge(O.create(sink => sig = sink && (() => sig = null)))
+    .merge(O.create(sink => (loop = sink) && (() => loop = null)))
     .share()
 
-  const {signals: s, effects: e} = main(input(input$))
-  const output$ = output({s: output(s, true), e: output(e)})
+  const res = main(signals(input$))
+  const all$ = mergeObj({in: mergeObj(res.in, true), out: mergeObj(res.out)})
 
-  const effect$ = O.create(eff => {
-    return output$.subscribe(
-      ({key, val}) => key === "e" ? eff.onNext(val) : sig && sig.onNext(val),
-      error => eff.onError(error),
-      () => eff.onCompleted()
+  const output$ = O.create(out => {
+    return all$.subscribe(
+      ({key, val}) => key === "out" ? out.onNext(val) : (loop && loop.onNext(val)),
+      error => out.onError(error),
+      () => out.onCompleted()
     )
   })
-  return input(effect$).keys(...keys(e))
+  return signals(output$).ofKeys(...keys(res.out))
 }
 
-export const interpret = (effect, interpreters) => {
+export const interpret = (effect, executors) => {
   return new Rx.CompositeDisposable(...keys(effect).map(key =>
-    interpreters[key] ? interpreters[key](effect[key]) : { dispose() {} }
+    executors[key] ? executors[key](effect[key]) : { dispose() {} }
   ))
+}
+
+export default (main, spec) => {
+  const {signals: s, transforms: t, executors: e} = drivers(spec)
+  return interpret(run(s, main(t)), e)
 }
--- test/coreTests.js
+++ test/coreTests.js
@@ -1,22 +1,22 @@
 import should from "should"
 import {Observable as O} from "rx"
-import tsers, {run, interpret} from "../src/index"
+import {drivers, run, interpret} from "../src/index"
 
 
-describe("tsers core", () => {
+describe("TSERS core", () => {
 
-  describe("initializer function", () => {
-    it("creates signals, transforms and interpreters from drivers", () => {
+  describe("drivers initialization", () => {
+    it("creates signals, transforms and executors from drivers", () => {
       const {driver: A, contents: eA} = makeDriver()
       const {driver: B, contents: eB} = makeDriver()
-      const {signals, transforms, interpreters} = tsers({A, B})
+      const {signals, transforms, executors} = drivers({A, B})
 
       signals.should.be.instanceof(O)
       should(transforms.A.foo === eA.transforms.foo).be.true()
       should(transforms.B.foo === eB.transforms.foo).be.true()
       should(transforms.A.foo === transforms.B.foo).be.false()
-      should(interpreters.A === eA.interpreter).be.true()
-      should(interpreters.B === eB.interpreter).be.true()
+      should(executors.A === eA.executor).be.true()
+      should(executors.B === eB.executor).be.true()
 
       function makeDriver() {
         const contents = {
@@ -25,7 +25,7 @@ describe("tsers core", () => {
             foo: () => {
             }
           },
-          interpreter: () => {
+          executor: () => {
           }
         }
         const driver = () => contents
@@ -33,23 +33,81 @@ describe("tsers core", () => {
       }
     })
     it("discards missing keys", () => {
-      const signals = O.just("a"), transforms = {}, interpreter = () => {}
+      const signals = O.just("a"), transforms = {}, executor = () => {}
       const A = () => ({signals, transforms})
-      const B = () => ({transforms, interpreter})
-      const C = () => ({signals, interpreter})
+      const B = () => ({transforms, executor})
+      const C = () => ({signals, executor})
 
-      const {transforms: t, interpreters: i} = tsers({A, B, C})
+      const {transforms: t, executors: i} = drivers({A, B, C})
       should(i.A).be.undefined()
       should(i.B).not.be.undefined()
       should(t.C).be.undefined()
       should(t.A).not.be.undefined()
     })
     it("allows creating env with no signals at all", done => {
-      const A = () => ({transforms: {}, interpreter: () => {}})
-      const {signals} = tsers({A})
+      const A = () => ({transforms: {}, executor: () => {}})
+      const {signals} = drivers({A})
       signals.subscribe(done.fail, done.fail, done)
+    })
+    it("merges signals and marks them with drivers key", done => {
+      const A = () => ({signals: O.just("a")})
+      const B = () => ({signals: O.just("b").delay(1)})
+      const {signals: s$} = drivers({A, B})
+      s$.bufferWithCount(2).subscribe(s => {
+        s.should.be.deepEqual([{key: "A", val: "a", local: false}, {key: "B", val: "b", local: false}])
+        done()
+      })
     })
   })
 
+  describe("run", () => {
+    const tsersDriver = () => ({
+      signals: O.just("tsers").delay(0)
+    })
+
+    it("is lazy", done => {
+      const {signals} = drivers({tsers: tsersDriver})
+      const out = run(signals, signals => ({
+        in: {
+          t: signals.of("tsers").do(done.fail)
+        },
+        out: {
+          msg: signals.of("t")
+        }
+      }))
+      out.msg.should.be.instanceof(O)
+      setTimeout(done, 10)
+    })
+    it("loops 'in' signals back to input and 'out' outside the system", done => {
+      const {signals} = drivers({tsers: tsersDriver})
+      const out = run(signals, signals => ({
+        in: {
+          t: signals.of("tsers").map(t => t + "!")
+        },
+        out: {
+          msg: signals.of("t").map(t => t + "!")
+        }
+      }))
+      out.msg.subscribe(t => {
+        t.should.equal("tsers!!")
+        done()
+      })
+    })
+    it("disposes the signal loop if the output stream is disposed", done => {
+      const {signals} = drivers({tsers: tsersDriver})
+      const out = run(signals, signals => ({
+        in: {
+          t: signals.of("tsers").delay(5).do(done.fail).merge(signals.of("tsers"))
+        },
+        out: {
+          msg: signals.of("t")
+        }
+      }))
+      const d = out.msg.subscribe(() => {
+        d.dispose()
+        setTimeout(done, 50)
+      })
+    })
+  })
 
 })
