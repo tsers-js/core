092a31506c6f83fb95bfbfe53deeeef6eaf8cc59
Matti Lankinen <m.lankinen@iki.fi>
2016-03-09 23:14:30 +0200

    Re-write core

--- src/index.js
+++ src/index.js
@@ -2,85 +2,90 @@ import Rx, {Observable as O} from "rx"
 
 const objKeys = x =>
   x ? Object.keys(x) : []
-const isObj = x =>
-  x && typeof x === "object" && x.constructor === Object
 
-const fromKey = (s$, key) =>
-  s$.filter(s => s.key === key).map(s => s.val)
+const isObj = x => typeof x === "object" && x.constructor === Object
 
-const fromKeys = (s$, ...keys) =>
-  keys.reduce((o, k) => ({...o, [k]: fromKey(s$, k)}), {})
-
-const withKey = (s$, key, ext = false) =>
-  s$.map(val => ({key, val, ext}))
-
-const withKeys = (objOfS$, ext = false) =>
-  O.merge(objKeys(objOfS$).map(k => withKey(objOfS$[k], k, ext)))
-
-const CommonSignalTransducers = {
-  fromKeys,
-  fromKey,
-  withKeys,
-  withKey
+const noop = () => {
 }
 
+const mapValuesWhen = (obj, fn) =>
+  objKeys(obj).reduce((o, k) => {
+    let a = fn(obj[k]);
+    return a ? ({...o, [k]: a}) : o
+  }, {})
 
-// drivers :: {A: () -> {s$, t, e}} -> {s$, t: {A: [t]}, e: {A: [e]}}
-export const drivers = spec => {
-  const mapValuesWhen = (obj, fn) =>
-    objKeys(obj).reduce((o, k) => { let a = fn(obj[k]); return a ? ({...o, [k]: a}) : o}, {})
 
-  if (!isObj(spec)) {
-    throw new Error("Drivers must be an object of initializer functions")
+function TSERS(drivers) {
+  if (!isObj(drivers)) throw new Error("Drivers must be an object of initializer functions")
+  const driverKeys = objKeys(drivers)
+  if (driverKeys.length === 0) throw new Error("At least one driver is required")
+
+  const from = (in$, key) =>
+    in$.filter(s => s.key === key).map(s => s.val)
+
+  const fromMany = (in$, ...keys) =>
+    keys.reduce((o, k) => ({...o, [k]: from(in$, k)}), {})
+
+  const to = (out$, key, ext = false) =>
+    out$.map(val => ({key, val, ext}))
+
+  const toMany = (objOf$, ext = false) =>
+    O.merge(...objKeys(objOf$).map(k => to(objOf$[k], k, ext)))
+
+  const decompose = (out$, ...extraKeys) =>
+    fromMany(out$, ...[...driverKeys, ...extraKeys])
+
+  const compose = objOf$ => toMany(objOf$, true)
+
+  const loop = (in$, main) => {
+    let lo = null
+    const loop$ = in$
+      .filter(s => s.ext)
+      .merge(O.create(o => (lo = o) && (() => lo = null)))
+      .share()
+
+    const [looped$, out$] = main(loop$)
+    const all$ = compose({lo: looped$ || O.never(), out: out$ || O.never()})
+
+    return O.create(out => {
+      return all$.subscribe(
+        ({key, val}) => key === "out" ? out.onNext(val) : (lo && lo.onNext(val)),
+        error => out.onError(error),
+        () => out.onCompleted()
+      )
+    }).share()
   }
-  const drivers =
-    mapValuesWhen(spec, f => f())
-  const signals =
-    withKeys(mapValuesWhen(drivers, d => d.signals), true)
-  const transducers = {
-    signal: CommonSignalTransducers,
-    ...mapValuesWhen(drivers, d => d.transducers)
+
+  const run = (in$, main, ...extraKeys) =>
+    decompose(loop(in$, main), ...extraKeys)
+
+
+  const CommonTransducers = {from, fromMany, to, toMany, compose, decompose, loop, run}
+  const DriverImports = CommonTransducers
+  const dds = mapValuesWhen(drivers, d => d(DriverImports))
+
+  const T = {
+    ...CommonTransducers,
+    ...mapValuesWhen(dds, d => d.transducers)
   }
+  const S =
+    compose(mapValuesWhen(dds, d => d.signals))
+
   const executors =
-    mapValuesWhen(drivers, d => d.executor)
-  return { signals, transducers, executors }
-}
+    mapValuesWhen(dds, d => d.executor)
 
-// run :: s$ -> (s$ -> {s$, o$}) -> o$
-export const run = (input$, main) => {
-  let lo = null
-  const inputWithLoop$ = input$
-    .filter(s => s.ext)
-    .merge(O.create(o => (lo = o) && (() => lo = null)))
-    .share()
+  if (objKeys(executors).length === 0) throw new Error("At least one executor is required")
 
-  const {loop, out} = main(inputWithLoop$)
-  const all$ = withKeys({loop: withKeys(loop), out: withKeys(out)})
-
-  const output$ = O.create(out => {
-    return all$.subscribe(
-      ({key, val}) => key === "out" ? out.onNext(val) : (lo && lo.onNext(val)),
-      error => out.onError(error),
-      () => out.onCompleted()
-    )
-  })
-  return fromKeys(output$.share(), ...objKeys(out))
-}
-
-const noopDispose = { dispose() {} }
-
-// execute :: {A: o$ -> dispose} -> {A: o$} -> dispose
-export const execute = (executors, output) => {
-  if (objKeys(executors).length === 0) {
-    throw new Error("At least one executor is required")
+  const E = function execute(output$) {
+    const noopd = {dispose: noop}
+    const out = decompose(output$)
+    return new Rx.CompositeDisposable(...objKeys(out).map(key =>
+      executors[key] ? executors[key](out[key]) || noopd : noopd
+    ))
   }
-  return new Rx.CompositeDisposable(...objKeys(output).map(key =>
-    executors[key] ? executors[key](output[key]) || noopDispose : noopDispose
-  ))
+
+  return [T, S, E] // R S
 }
 
-// for concise
-export default (spec, main) => {
-  const {signals: s, transducers: t, executors: e} = drivers(spec)
-  return execute(e, run(s, main(t)))
-}
+module.exports = TSERS
+module.exports.default = TSERS
