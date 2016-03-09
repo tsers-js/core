f41855580fdc7b0e176d1c599ec6b12a8fde79ce
Matti Lankinen <m.lankinen@iki.fi>
2016-03-09 13:26:41 +0200

    Refactor API contract

--- src/index.js
+++ src/index.js
@@ -1,64 +1,86 @@
 import Rx, {Observable as O} from "rx"
 
-const keys = Object.keys
+const objKeys = x =>
+  x ? Object.keys(x) : []
+const isObj = x =>
+  x && typeof x === "object" && x.constructor === Object
 
-const fromKey = signal$ => key =>
-  signal$.filter(s => s.key === key).map(s => s.val)
+const fromKey = (s$, key) =>
+  s$.filter(s => s.key === key).map(s => s.val)
 
+const fromKeys = (s$, ...keys) =>
+  keys.reduce((o, k) => ({...o, [k]: fromKey(s$, k)}), {})
 
-const mergeObj = (output, local = false) =>
-  O.merge(keys(output).map(key => output[key].map(val => ({val, key, local}))))
+const withKey = (s$, key, ext = false) =>
+  s$.map(val => ({key, val, ext}))
 
-const signals = signal$ => ({
-  of: fromKey(signal$),
-  ofKeys: (...keys) => keys.reduce((o, k) => ({...o, [k]: fromKey(signal$)(k)}), {})
-})
+const withKeys = (objOfS$, ext = false) =>
+  O.merge(objKeys(objOfS$).map(k => withKey(objOfS$[k], k, ext)))
+
+const CommonSignalTransducers = {
+  fromKeys,
+  fromKey,
+  withKeys,
+  withKey
+}
+
 
 // drivers :: {A: () -> {s$, t, e}} -> {s$, t: {A: [t]}, e: {A: [e]}}
 export const drivers = spec => {
-  const compact = (obj, fn) =>
-    keys(obj).reduce((o, k) => { let a = fn(obj[k]); return a ? ({...o, [k]: a}) : o}, {})
+  const mapValuesWhen = (obj, fn) =>
+    objKeys(obj).reduce((o, k) => { let a = fn(obj[k]); return a ? ({...o, [k]: a}) : o}, {})
 
-  const drivers = compact(spec, f => f())
-  return {
-    signals: mergeObj(compact(drivers, d => d.signals), false),
-    transforms: compact(drivers, d => d.transforms),
-    executors: compact(drivers, d => d.executor)
+  if (!isObj(spec)) {
+    throw new Error("Drivers must be an object of initializer functions")
   }
+  const drivers =
+    mapValuesWhen(spec, f => f())
+  const signals =
+    withKeys(mapValuesWhen(drivers, d => d.signals), true)
+  const transducers = {
+    signal: CommonSignalTransducers,
+    ...mapValuesWhen(drivers, d => d.transducers)
+  }
+  const executors =
+    mapValuesWhen(drivers, d => d.executor)
+  return { signals, transducers, executors }
 }
 
 // run :: s$ -> (s$ -> {s$, o$}) -> o$
-export const run = (signal$, main) => {
-  let loop = null
-  const input$ = signal$
-    .filter(s => !s.local)
-    .merge(O.create(sink => (loop = sink) && (() => loop = null)))
+export const run = (input$, main) => {
+  let lo = null
+  const inputWithLoop$ = input$
+    .filter(s => s.ext)
+    .merge(O.create(o => (lo = o) && (() => lo = null)))
     .share()
 
-  const res = main(signals(input$))
-  const all$ = mergeObj({loop: mergeObj(res.in || {}, true), out: mergeObj(res.out || {})})
+  const {loop, out} = main(inputWithLoop$)
+  const all$ = withKeys({loop: withKeys(loop), out: withKeys(out)})
 
   const output$ = O.create(out => {
     return all$.subscribe(
-      ({key, val}) => key === "out" ? out.onNext(val) : (loop && loop.onNext(val)),
+      ({key, val}) => key === "out" ? out.onNext(val) : (lo && lo.onNext(val)),
       error => out.onError(error),
       () => out.onCompleted()
     )
   })
-  return signals(output$).ofKeys(...keys(res.out || {}))
+  return fromKeys(output$.share(), ...objKeys(out))
 }
 
 const noopDispose = { dispose() {} }
 
 // execute :: {A: o$ -> dispose} -> {A: o$} -> dispose
 export const execute = (executors, output) => {
-  return new Rx.CompositeDisposable(...keys(output).map(key =>
+  if (objKeys(executors).length === 0) {
+    throw new Error("At least one executor is required")
+  }
+  return new Rx.CompositeDisposable(...objKeys(output).map(key =>
     executors[key] ? executors[key](output[key]) || noopDispose : noopDispose
   ))
 }
 
 // for concise
 export default (spec, main) => {
-  const {signals: s, transforms: t, executors: e} = drivers(spec)
+  const {signals: s, transducers: t, executors: e} = drivers(spec)
   return execute(e, run(s, main(t)))
 }
