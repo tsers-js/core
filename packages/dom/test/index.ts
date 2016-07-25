c0fc3dd1e99c9c46272d9842781f3e8bba9ea34a
Matti Lankinen <m.lankinen@iki.fi>
2016-07-26 01:54:51 +0300

    Create DOM package as an example

---
+++ packages/dom/test/index.ts
@@ -0,0 +1,9 @@
+/// <reference path="../typings/index.d.ts" />
+import * as assert from "power-assert"
+import index from "../src/index"
+
+describe("index default export", () => {
+  it("should return package name", () => {
+    assert(index() === "dom")
+  })
+})
