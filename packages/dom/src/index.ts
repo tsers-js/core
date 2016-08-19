f4598ff83b871db0affbb9674f2099b4b3f74485
Matti Lankinen <m.lankinen@iki.fi>
2016-08-19 10:16:49 +0300

    Remove obsolete DOM package

--- packages/dom/src/index.ts
+++
@@ -1,3 +0,0 @@
-export default function index(): string {
-  return "dom"
-}
--- packages/dom/test/index.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="../typings/index.d.ts" />
-import * as assert from "power-assert"
-import index from "../src/index"
-
-describe("index default export", () => {
-  it("should return package name", () => {
-    assert(index() === "dom")
-  })
-})
