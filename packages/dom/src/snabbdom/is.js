1f662be0fb2a9465b7b832b7a9536d11f511e207
Matti Lankinen <m.lankinen@iki.fi>
2016-10-05 20:34:10 +0300

    Remove snabbdom

--- packages/dom/src/snabbdom/is.js
+++
@@ -1,9 +0,0 @@
-/* eslint semi: 0, quotes: 0 */
-import {isArray} from "@tsers/core"
-
-module.exports = {
-  array: isArray,
-  primitive: function (s) {
-    return typeof s === 'string' || typeof s === 'number';
-  }
-};
